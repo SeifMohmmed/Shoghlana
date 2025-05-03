a0ee43d76c43baa0699a159f9fb8e4f32d4d8cdc
Seif Mohamed <seif.eissa7797@gmail.com>
2025-05-04 00:14:12 +0300

    Add JobController and RateController

---
+++ Shoghlana.API/Controllers/JobController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shoghlana.API.Response;
+using Shoghlana.Core.Interfaces;
+using Shoghlana.Core.Models;
+
+namespace Shoghlana.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class JobController : ControllerBase
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public JobController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    [HttpGet]
+    public ActionResult<GeneralResponse> GetAll()
+    {
+        var jobs=_unitOfWork.job.GetAll().ToList();
+
+        return new GeneralResponse
+        {
+            IsSuccess = true,
+            Data = jobs,
+            Message = "All jobs retrieved successfully"
+        };
+
+    }
+    [HttpGet("id")]
+    public ActionResult<GeneralResponse> GetById(int id)
+    {
+        Job job = new Job();
+
+        try
+        {
+            job=_unitOfWork.job.GetById(id);
+        }
+        catch (Exception ex)
+        {
+            return new GeneralResponse()
+            {
+                IsSuccess = false,
+                Data = null,
+                Message = ex.Message
+            };
+        }
+        return new GeneralResponse
+        {
+            IsSuccess = true,
+            Data = job,
+            Message = "Job is retrieved successfully"
+        };
+    }
+}
---
+++ Shoghlana.API/Controllers/RateController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shoghlana.Core.Interfaces;
+
+namespace Shoghlana.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class RateController : ControllerBase
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RateController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+}
